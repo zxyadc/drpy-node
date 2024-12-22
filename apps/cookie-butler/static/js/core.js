774d7d2b40a5f983f7f7332eb4f1dcab5c244d8e
Hiram <>
2024-12-22 10:00:45 +0800

    [optimize] crypto.randomUUID compatibility

--- apps/cookie-butler/static/js/core.js
+++ apps/cookie-butler/static/js/core.js
@@ -29,7 +29,13 @@ class QRCodeHandler {
     }
 
     static generateUUID() {
-        return crypto.randomUUID();
+        if (crypto && typeof crypto.randomUUID === 'function') {
+            return crypto.randomUUID();
+        } else {
+            return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+                (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+            );
+        }
     }
 
     async _generateQRCode(url) {
