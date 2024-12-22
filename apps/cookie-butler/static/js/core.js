ff2f72880f18e3c79b8e00ea27e9287c81a83a47
Hiram <>
2024-12-22 10:05:18 +0800

    [optimize] crypto.getRandomValues judgements

--- apps/cookie-butler/static/js/core.js
+++ apps/cookie-butler/static/js/core.js
@@ -31,10 +31,16 @@ class QRCodeHandler {
     static generateUUID() {
         if (crypto && typeof crypto.randomUUID === 'function') {
             return crypto.randomUUID();
-        } else {
+        } else if (crypto && typeof crypto.getRandomValues === 'function') {
             return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
                 (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
             );
+        } else {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                const r = (Math.random() * 16) | 0,
+                  v = c == 'x' ? r : (r & 0x3) | 0x8;
+                return v.toString(16);
+              });
         }
     }
 
